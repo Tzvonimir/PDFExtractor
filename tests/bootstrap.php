95731a6a496012fe8df52ece2f8f33c690819545
Zvonimir Tomesic <t.zvonimir@gmail.com>
2018-02-17 23:49:36 +0000

    Add autoload

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,3 +5,5 @@ $oldClass = '\PHPUnit_Framework_TestCase';
 if (!class_exists($newClass) && class_exists($oldClass)) {
     class_alias($oldClass, $newClass);
 }
+
+require __DIR__ . '/../vendor/autoload.php';
