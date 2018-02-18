e071265c6b2e848b9651deb4cc167c300721b403
Zvonimir Tomesic <t.zvonimir@gmail.com>
2018-02-18 00:03:12 +0000

    Fix test case

--- tests/PDFExtractorTest.php
+++ tests/PDFExtractorTest.php
@@ -5,9 +5,13 @@ use ZTomesic\PDFExtractor\PDFExtractor;
 class PDFExtractorTest extends \PHPUnit\Framework\TestCase
 {
 
+    public function setUp()
+    {
+    }
+
     public function testShouldCreateDirectory() {
         PDFExtractor::createDirectory(__DIR__ . '/test/');
-        $this->assertDirectoryExists(__DIR__ . '/test/');
+        $this->assertFileExists(__DIR__ . '/test/');
     }
 
     protected function getTestDocument()
