0915f576e0d60f73250ac2a877055a05b1f3e2d3
t.zvonimir@gmail.com <t.zvonimir@gmail.com>
2018-02-24 14:34:49 +0000

    Fix constructor

--- src/Builder.php
+++ src/Builder.php
@@ -12,8 +12,6 @@ class Builder
 
     protected static $_instance = null;
 
-    protected function __construct () { }
-
     public static function getInstance ()
     {
         if (self::$_instance === null) {
